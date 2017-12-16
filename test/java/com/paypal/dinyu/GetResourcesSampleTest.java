be3a953649d247f0f61777ea4ce742d7f9b4f4ba
dinyu <dinyu@paypal.com>
2017-12-16 15:42:18 +0800

    GetResourcesSample

---
+++ test/java/com/paypal/dinyu/GetResourcesSampleTest.java
@@ -0,0 +1,48 @@
+package com.paypal.dinyu;
+
+import junit.framework.TestCase;
+
+import java.io.InputStream;
+import java.text.MessageFormat;
+
+public class GetResourcesSampleTest extends TestCase {
+    private static final String MAIN_FILE = "main-resources-file.txt";
+    private static final String MAIN_FILE_WITH_SLASH = "/main-resources-file.txt";
+    private static final String TEST_FILE = "test-resources-file.txt";
+    private static final String TEST_FILE_WITH_SLASH = "/test-resources-file.txt";
+    private static final String ROOT_FILE = "project-root-file.txt";
+    private static final String ROOT_FILE_WITH_SLASH = "/project-root-file.txt";
+
+    public void testGetResources() {
+        validateResult(MAIN_FILE);
+        validateResult(MAIN_FILE_WITH_SLASH);
+
+        validateResult(TEST_FILE);
+        validateResult(TEST_FILE_WITH_SLASH);
+
+        validateResult(ROOT_FILE);
+        validateResult(ROOT_FILE_WITH_SLASH);
+
+    }
+
+    private static void validateResult(String file) {
+
+        InputStream inputStream1 = GetResourcesSample.class.getClass().getResourceAsStream(file);
+        if (inputStream1 != null) {
+            System.out.println(MessageFormat
+                    .format("GetResourcesSample.class.getClass().getResourceAsStream(\"{0}\") is not null!", file));
+        }
+
+        InputStream inputStream2 = GetResourcesSample.class.getResourceAsStream(file);
+        if (inputStream2 != null) {
+            System.out.println(
+                    MessageFormat.format("GetResourcesSample.class.getResourceAsStream(\"{0}\") is not null!", file));
+        }
+
+        InputStream inputStream3 = GetResourcesSample.class.getClassLoader().getResourceAsStream(file);
+        if (inputStream3 != null) {
+            System.out.println(
+                    MessageFormat.format("GetResourcesSample.class.getClassLoader(\"{0}\") is not null!", file));
+        }
+    }
+}
